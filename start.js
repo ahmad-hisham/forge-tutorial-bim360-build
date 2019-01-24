4df00a04441fc554577d17f6c1a5b2cd34febd31
Ahmad Hisham <ahmad.hisham@gmail.com>
2019-01-24 20:41:43 +0300

    Sign-in to Autodesk account using 3-legged OAuth

--- start.js
+++ start.js
@@ -11,4 +11,17 @@ app.set("view engine", "ejs");
 var indexRouter = require("./routes/index");
 app.use("/", indexRouter);
 
+// cookie-based session
+var cookieSession = require("cookie-session");
+app.use(cookieSession({
+  name: "forgesession",
+  keys: ["forgesecurekey"],
+  httpOnly: false,
+  maxAge: 14 * 24 * 60 * 60 * 1000 // 14 days, same as refresh token
+}));
+
+// OAuth 3-legged route
+var authRouter = require("./routes/oauth");
+app.use("/oauth", authRouter);
+
 app.listen(3000);
